16b490d35cf3d6529a5cf91bb24cc509aadc6fb0
Mykolas Kvieska <mykolaska@outlook.com>
2018-04-24 16:21:43 +0300

    Add Ant Design(React UI Library)

--- src/components/Owner/index.js
+++ src/components/Owner/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import { Layout, Row, Col } from 'antd'
 
 import './index.css'
 
+const { Header, Content } = Layout
 
 class Owner extends React.Component {
   render () {
     return (
-      <div className='onwer-page'>
+      <Layout className='onwer-page'>
+        <Header className='onwer-page__header'>
           <h2 className='page-title'>Owners List Page</h2>
-      </div>
+        </Header>
+        <Content className='onwer-page__content'>
+          <Row>
+            <Col md={16}>
+              Table
+            </Col>
+            <Col md={8}>
+              Map
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     )
   }
 }
