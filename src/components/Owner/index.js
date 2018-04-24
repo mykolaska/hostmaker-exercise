07315f107f217e7620a42254afda5fe330a4d3f5
Mykolas Kvieska <mykolaska@outlook.com>
2018-04-24 17:24:53 +0300

    Render onwers as a table from the api

--- src/components/Owner/index.js
+++ src/components/Owner/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Layout, Row, Col } from 'antd'
+import PropTypes from 'prop-types'
+import { Layout, Row, Col, message } from 'antd'
+import OwnerTable  from '../OwnerTable'
 
 import './index.css'
 
 const { Header, Content } = Layout
 
 class Owner extends React.Component {
+  static propTypes = {
+    owners: PropTypes.array,
+    ownersLoading: PropTypes.bool,
+    ownersError: PropTypes.bool,
+    ownersErrorMessage: PropTypes.string,
+    getOwners: PropTypes.func,
+  }
+
+  componentWillMount () {
+    this.props.getOwners()
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.ownersError) {
+      message.error(nextProps.ownersErrorMessage)
+    }
+  }
+
   render () {
+    const { owners, ownersLoading } = this.props
     return (
       <Layout className='onwer-page'>
         <Header className='onwer-page__header'>
@@ -15,7 +36,7 @@ class Owner extends React.Component {
         <Content className='onwer-page__content'>
           <Row>
             <Col md={16}>
-              Table
+              <OwnerTable owners={owners} ownersLoading={ownersLoading} />
             </Col>
             <Col md={8}>
               Map
--- src/containers/Owner/index.js
+++ src/containers/Owner/index.js
@@ -6,6 +6,10 @@ import Owner  from '../../components/Owner'
 const mapDispatchToProps = (dispatch) => bindActionCreators(operations, dispatch)
 
 const mapStateToProps = (state) => ({
+  owners: state.owners ? state.owners.data : [],
+  ownersLoading: state.owners ? state.owners.isLoading : false,
+  ownersError: state.owners ? state.owners.error : false,
+  ownersErrorMessage: state.owners ? state.owners.errorMessage : null,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Owner)
