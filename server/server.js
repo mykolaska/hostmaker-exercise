699d32a3392aa74cd2e90299f3cd4c6300d5a532
Mykolas Kvieska <mykolaska@outlook.com>
2018-04-24 17:21:23 +0300

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const exerciseData = require('./data.json')
 
-app.get('/api/v1/owners', (req, res) => res.send(exerciseData))
+var whitelist = ['http://localhost:3000', 'http://localhost:3001']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-app.listen(3001, () => console.log('Hostmaker exercise Index listening on port 3001!'))
+app.get('/api/v1/owners', cors(corsOptions), (req, res) => res.send(exerciseData))
+
+app.listen(3001, () => console.log('Hostmaker exercise listening on port 3001!'))
